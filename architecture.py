c9e70a4b18c8f4fa7ad870ebe093f95e20f2b304
z3y50n <dim_papag@windowslive.com>
2021-09-19 12:47:35 +0300

    We like enums

--- architecture.py
+++ architecture.py
@@ -1,4 +1,30 @@
 import struct
+from enum import Enum
+
+
+class OpCodes(Enum):
+    HALT = 0
+    SET = 1
+    PUSH = 2
+    POP = 3
+    EQ = 4
+    GT = 5
+    JMP = 6
+    JT = 7
+    JF = 8
+    ADD = 9
+    MULT = 10
+    MOD = 11
+    AND = 12
+    OR = 13
+    NOT = 14
+    RMEM = 15
+    WMEM = 16
+    CALL = 17
+    RET = 18
+    OUT = 19
+    IN = 20
+    NOOP = 21
 
 
 class Registers:
--- synacor.py
+++ synacor.py
@@ -1,33 +1,9 @@
-from architecture import Memory, Registers, Stack
+from architecture import Memory, Registers, Stack, OpCodes
 
 memory = Memory()
 registers = Registers()
 stack = Stack()
 PC = 0
-commands = {
-    0: "halt",
-    1: "set",
-    2: "push",
-    3: "pop",
-    4: "eq",
-    5: "gt",
-    6: "jmp",
-    7: "jt",
-    8: "jf",
-    9: "add",
-    10: "mult",
-    11: "mod",
-    12: "and",
-    13: "or",
-    14: "not",
-    15: "rmem",
-    16: "wmem",
-    17: "call",
-    18: "ret",
-    19: "out",
-    20: "in",
-    21: "noop",
-}
 
 
 def read_code(filename):
@@ -37,52 +13,50 @@ def read_code(filename):
 
 
 def display(cmd):
-    print(f"Cmd: {cmd}")
-    print(f"PC: {PC:5}")
+    print(f"PC: {PC:5}, Command: {cmd}")
     print(registers)
 
 
 def execute():
     global PC
-    opcode = memory[PC]
+    opcode = OpCodes(memory[PC])
+    cmd = opcode.name
     a = memory[PC + 1]
     b = memory[PC + 2]
     c = memory[PC + 3]
-    cmd = commands[opcode]
 
-    if opcode == 0:
+    if opcode == OpCodes.HALT:
         display(cmd)
-        print("Halt")
+        print("Halt. Exiting...")
         exit(0)
-    elif opcode == 6:
+    elif opcode == OpCodes.JMP:
         display(cmd)
         PC = a
-    elif opcode == 7:
+    elif opcode == OpCodes.JT:
         display(cmd)
         if a:
             PC = b
         else:
             PC += 1
-    elif opcode == 8:
+    elif opcode == OpCodes.JF:
         display(cmd)
         if not a:
             PC = b
         else:
             PC += 1
-    elif opcode == 19:
+    elif opcode == OpCodes.OUT:
         print(chr(a), end="")
         PC += 2
-    elif opcode == 21:
+    elif opcode == OpCodes.NOOP:
         PC += 1
     else:
-        print(f"Didn't implement logic for opcode {opcode}")
+        print(f"Didn't implement logic for command {cmd} with opcode {opcode}")
         exit(0)
 
 
 if __name__ == "__main__":
     code = read_code("challenge.bin")
     memory.load_code(code)
-    PC = 487
 
     while PC < len(memory):
         execute()
